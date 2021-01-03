8216aee55346d5762997f7ab3d2746297dd78684
Dropship Lite <70704616+dropship-lite@users.noreply.github.com>
2021-01-03 13:01:14 +0800

    updated

---
+++ web-based/admin/dashboard.js
@@ -0,0 +1,13 @@
+function onMouseOver(){
+    var tooltip = document.getElementById('tooltip')
+    tooltip.style.display = 'block'
+}
+
+function onMouseOut(){
+    var tooltip = document.getElementById('tooltip')
+    tooltip.style.display = 'none'
+}
+
+function logout(){
+
+}
